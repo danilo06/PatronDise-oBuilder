ef2c6420ef2658a89c6d5ce523d294f4d232dc10
DaniloBeleño <dbeleno86@uan.edu.co>
2019-03-05 19:20:44 -0500

    Patron Singleton Terminado

---
+++ singleton/src/main/java/ingsw/pdd/singleton/ConfigurationSingleton.java
@@ -0,0 +1,64 @@
+package ingsw.pdd.singleton;
+
+import java.util.Properties;
+import ingsw.pdd.singleton.util.PropertiesUtil;
+
+public class ConfigurationSingleton {
+
+	private static ConfigurationSingleton singleton;
+
+	private static final String CONFIGURATION_PROP = "META-INF/Configuration.properties";
+
+	private static final String APP_NAME_PROP = "appName";
+	private static final String APP_VERSION_PROP = "appVersion";
+
+	private String appName;
+	private String appVersion;
+
+	private ConfigurationSingleton() {
+		Properties prop = PropertiesUtil.loadProperty(CONFIGURATION_PROP);
+		this.appName = prop.getProperty(APP_NAME_PROP);
+		this.appVersion = prop.getProperty(APP_VERSION_PROP);
+	}
+
+	private static synchronized void createInstance() {
+		if (singleton == null) {
+			singleton = new ConfigurationSingleton();
+		}
+	}
+
+	public static ConfigurationSingleton getInstance() {
+		if (singleton == null) {
+			createInstance();
+		}
+		return singleton;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	public static void setSingleton(ConfigurationSingleton singleton) {
+		ConfigurationSingleton.singleton = singleton;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfigurationSingleton [appName=" + appName + ", appVersion=" + appVersion + "]";
+	}
+	
+	
+	
+}
